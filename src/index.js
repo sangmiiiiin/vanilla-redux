82558a0782949be7e896fc93adb18beb0c87961e
SANGMIN <kwk680037@gmail.com>
2023-11-21 18:06:52 +0900

    swichcase, variable, type

--- src/index.js
+++ src/index.js
@@ -4,13 +4,18 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
+const ADD = "ADD"
+const MINUS = "MINUS"
+
+
 const countModifider = (count = 0, action) => {
-    if (action.type === "ADD") {
-        return count + 1;
-    } else if (action.type === "MINUS"){
-        return count - 1;
-    } else {
-        return count;    
+    switch (action.type) {
+        case ADD:
+            return count + 1
+        case MINUS:
+            return count - 1
+        default:
+            return count;
     }
 };
 
@@ -22,11 +27,11 @@ const onChange = () => {
 countStore.subscribe(onChange);
 
 const handleAdd = () => {
-    countStore.dispatch( {type: "ADD"} )
+    countStore.dispatch( {type: ADD} )
 ;}
 
 const handleMinus = () => {
-    countStore.dispatch( {type: "MINUS"} )
+    countStore.dispatch( {type: MINUS} )
 };
 
 add.addEventListener("click", handleAdd);
