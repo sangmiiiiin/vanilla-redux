1fa4451f513bf60f1cd84137e22e64ee848ff03b
SANGMIN <kwk680037@gmail.com>
2023-11-21 09:57:28 +0900

    action, dispatch, getState

--- src/index.js
+++ src/index.js
@@ -1,25 +1,28 @@
+import { legacy_createStore } from "redux";
+
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-let count = 0;
-
-number.innerText = count;
-
-const updateText = () => {
-    number.innerText = count;
+const countModifider = (count = 0, action) => {
+    console.log(count, action);
+    if (action.type === "ADD") {
+        return count + 1;
+    } else if (action.type === "MINUS"){
+        return count - 1;
+    } else {
+        return count;    
+    }
 };
 
-const handleAdd = () => {
-    count = count + 1;
-    updateText();
-};
+const countStore = legacy_createStore(countModifider);
 
-const handleMinus = () => {
-    count = count - 1;
-    updateText();
-};
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "MINUS" });
+countStore.dispatch({ type: "MINUS" });
+countStore.dispatch({ type: "MINUS" });
 
-
-add.addEventListener("click", handleAdd)
-minus.addEventListener("click", handleMinus)
+console.log(countStore.getState());
