bf1462a851fa28d45f3888fd0757b773797a58a9
SANGMIN <kwk680037@gmail.com>
2023-11-21 15:57:22 +0900

    subscribe

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
 const countModifider = (count = 0, action) => {
-    console.log(count, action);
     if (action.type === "ADD") {
         return count + 1;
     } else if (action.type === "MINUS"){
@@ -17,12 +16,18 @@ const countModifider = (count = 0, action) => {
 
 const countStore = legacy_createStore(countModifider);
 
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "MINUS" });
-countStore.dispatch({ type: "MINUS" });
-countStore.dispatch({ type: "MINUS" });
+const onChange = () => {
+    number.innerText = countStore.getState();
+}
+countStore.subscribe(onChange);
 
-console.log(countStore.getState());
+const handleAdd = () => {
+    countStore.dispatch( {type: "ADD"} )
+;}
+
+const handleMinus = () => {
+    countStore.dispatch( {type: "MINUS"} )
+};
+
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);
